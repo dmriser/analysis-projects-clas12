3aa93d96d3846008333adbd184654d9b011bfef8
david-riser <dmriser@gmail.com>
2019-11-04 20:15:13 -0500

    Comparing data/sim

--- projects/elastic/python/makePlots.sh
+++ projects/elastic/python/makePlots.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#python data_sim_lite.py -d=mon-lite-rga-in.root -s=mon-lite-sim02-in.root -o=compare-in
-python mon.py -i=rga-in.root -o=rga-in
+python data_sim_lite.py -d=rga-in-lite.hipo.root -s=esepp-lite.hipo.root -o=compare-in
+#python mon.py -i=rga-in.root -o=rga-in
+#python mon.py -i=esepp.root -o=esepp
 #python pres.py -i=rga-in.root -o=rga-in
-#python mon.py -i=rga-out.root -o=rga-out
-#python mon.py -i=sim04-in.root -o=sim04-in
 
-#python data_sim.py -d=rga-in.root -s=sim04-in.root -o=compare-in
+#python data_sim.py -d=rga-in.root -s=esepp.root -o=compare-in
 #python data_sim.py -d=rga-out.root -s=sim04-in.root -o=compare-out
