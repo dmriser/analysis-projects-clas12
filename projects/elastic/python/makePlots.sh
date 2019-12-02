d8c7ed3851105b1646b4a60455bb45b4d09896d0
David Riser <davidriser@Davids-iMac.local>
2019-12-02 08:54:46 -0500

    Adding monitoring code for rad events

--- projects/elastic/python/makePlots.sh
+++ projects/elastic/python/makePlots.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-python data_sim.py -d=rga-in.root -s=esepp.root -o=compare-in
+#python data_sim.py -d=rga-in.root -s=esepp.root -o=compare-in
 #python mon.py -i=rga-in.root -o=rga-in
 #python mon.py -i=esepp.root -o=esepp
+python monrad.py -i=esepp-rad.hipo.root -o=esepp-rad
 
 
