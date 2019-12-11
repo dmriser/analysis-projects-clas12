f354951f57eb5806efc2c3fd248c7a3aa8388866
David Riser <davidriser@Davids-iMac.local>
2019-12-11 09:24:43 -0500

    plotting scripts updated

--- projects/elastic/python/makePlots.sh
+++ projects/elastic/python/makePlots.sh
@@ -3,7 +3,7 @@
 #python data_sim.py -d=rga-in.root -s=esepp.root -o=compare-in
 #python mon.py -i=rga-in.root -o=rga-in
 #python mon.py -i=esepp.root -o=esepp
-python monrad.py -i=mon-rad.hipo.root -o=rga-rad
-python monrad.py -i=esepp-rad.hipo.root -o=esepp-rad
+python monrad.py -i=rga-rad.root -o=rga-rad
+python monrad.py -i=esepp-rad.root -o=esepp-rad
 
 
