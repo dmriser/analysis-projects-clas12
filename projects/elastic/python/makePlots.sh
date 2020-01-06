829daa7ebd796c27e6077d13a901dc2c6b11dd42
David Riser <davidriser@Davids-iMac.local>
2020-01-06 13:03:06 -0500

    Stashing local changes

--- projects/elastic/python/makePlots.sh
+++ projects/elastic/python/makePlots.sh
@@ -2,8 +2,9 @@
 
 #python data_sim.py -d=rga-in.root -s=esepp.root -o=compare-in
 python mon.py -i=rga.root -o=rga
-python mon.py -i=esepp.root -o=esepp
-python monrad.py -i=rga-rad.root -o=rga-rad
-python monrad.py -i=esepp-rad.root -o=esepp-rad
+python mon.py -i=markov.root -o=markov
+#python mon.py -i=esepp.root -o=esepp
+#python monrad.py -i=rga-rad.root -o=rga-rad
+#python monrad.py -i=esepp-rad.root -o=esepp-rad
 
 
