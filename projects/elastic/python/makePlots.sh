b619d25a9f56e143cff2500e579a7a5647c9105d
David Riser <davidriser@Davids-iMac.local>
2020-01-06 14:59:56 -0500

    plotting

--- projects/elastic/python/makePlots.sh
+++ projects/elastic/python/makePlots.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #python data_sim.py -d=rga-in.root -s=esepp.root -o=compare-in
-python mon.py -i=rga.root -o=rga
-python mon.py -i=markov.root -o=markov
+#python mon.py -i=rga.root -o=rga
+#python mon.py -i=markov.root -o=markov
 #python mon.py -i=esepp.root -o=esepp
 #python monrad.py -i=rga-rad.root -o=rga-rad
 #python monrad.py -i=esepp-rad.root -o=esepp-rad
+
+python es.py -d=es-rga.root -s=es-esepp.root -o=es
 
 
