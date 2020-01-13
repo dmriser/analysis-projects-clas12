51b577fc33ccbbd2a72bd3f7ad0ffa20d0049ecb
David Riser <davidriser@Davids-iMac.local>
2020-01-13 11:28:02 -0500

    making problems

--- projects/elastic/python/makePlots.sh
+++ projects/elastic/python/makePlots.sh
@@ -4,9 +4,9 @@
 #python mon.py -i=rga.root -o=rga
 #python mon.py -i=markov.root -o=markov
 #python mon.py -i=esepp.root -o=esepp
-#python monrad.py -i=rga-rad.root -o=rga-rad
-#python monrad.py -i=esepp-rad.root -o=esepp-rad
+python monrad.py -i=rga-rad.root -o=rga-rad
+python monrad.py -i=esepp-rad.root -o=esepp-rad
 
-python es.py -d=es-rga.root -s=es-esepp.root -o=es
+#python es.py -d=es-rga.root -s=es-esepp.root -o=es
 
 
